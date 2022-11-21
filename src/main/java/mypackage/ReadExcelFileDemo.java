4f3cfe1ffac174a16a9794d42b7a8913159c1ad1
lincyshinu <118542167+lincyshinu@users.noreply.github.com>
2022-11-21 19:21:12 +0400

    initialCommit

---
+++ src/main/java/mypackage/ReadExcelFileDemo.java
@@ -0,0 +1,31 @@
+package mypackage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelFileDemo {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File f=new File("C:\\Users\\Shinu\\Desktop\\stud.xlsx");
+		FileInputStream fis=new FileInputStream(f);
+		XSSFWorkbook wb=new XSSFWorkbook(fis);
+		XSSFSheet sheet=wb.getSheetAt(0);
+		
+		for(Row r:sheet)
+		{
+			for(Cell c:r) {
+				System.out.println(c);
+			}
+				
+		}
+		wb.close();
+	}
+}
